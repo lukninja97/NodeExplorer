c6abf136c843eab1c4b84820ab0ac29c53586e3b
Lucas <lucassilvasouto@gmail.com>
2024-08-24 18:10:33 -0300

    Add and configure retrofit

---
+++ app/src/main/java/com/lukninja/nodeexplorer/service/repository/remote/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package com.lukninja.nodeexplorer.service.repository.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient private constructor() {
+    companion object {
+        private lateinit var retrofit: Retrofit
+
+        private fun getRetrofitInstance(): Retrofit {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+
+            val httpClient = OkHttpClient.Builder().addInterceptor(logging).build()
+
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl("https://mempool.space/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient)
+                    .build()
+            }
+
+            return retrofit
+        }
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
+}
